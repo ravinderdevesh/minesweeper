3123aaa80389ccb6bf39f219ec09c33bc664dc9c
Rameshwar Bhaskaran <rameshwar.zorro@gmail.com>
2015-03-06 12:52:44 +0530

    Updated comment header..work to start soon

---
+++ minesweeper.cpp
@@ -0,0 +1,15 @@
+/*
+MINESWEEPER GAME IN C++
+
+Created By :Rameshwar Bhaskaran
+Date: 6th March 2015 
+
+You are free to distribute,copy,modify and use the code as long as this comment header stays intact .
+You may modify the code ,but please do cite the original source.
+
+DISCLAIMER: The author of this code is not responsible for anything wrong that may happen by executing this code 
+
+*/
+#include<iostream>
+#include<allegro.h>
+
